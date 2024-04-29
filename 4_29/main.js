6bc4f3fc449422b074773e23eb57570d752f9c49
jsb5324 <jsb5324@naver.com>
2024-04-29 21:12:20 +0900

    4_29

---
+++ 4_29/main.js
@@ -0,0 +1,25 @@
+let time = prompt("시간을 입력하세요");
+
+if
+
+let money = prompt("숫자를 입력하세요.");
+
+won = exchange * 1300;
+
+//자료형 숫자 문자 불린
+let A = "aaa";
+console.log(typeof(A));
+
+//"+" 문자열 연산, 숫자 연산
+let a = "가나다"+"라마바사";
+console.log(a);
+// a 가나다라마바사
+let num = 123+456;
+console.log 
+
+// mola
+let score = Number(prompt('학점을 입력하세요', '학점'));
+
+if(score = 4.5) 
+
+
